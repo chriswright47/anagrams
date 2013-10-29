1b98a1768075c5291ff781ce2e657aafa9623f34
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 19:30:55 -0500

    .

--- sinatra_skeleton/app/models/word.rb
+++ sinatra_skeleton/app/models/word.rb
@@ -2,13 +2,14 @@ class Word < ActiveRecord::Base
 
   def self.anagrams(test_word)
     words_array = []
-    Word.all.each do |word|
-      if word.ordered == test_word.chars.sort.join.downcase
+    Word.where("ordered= ?", test_word.chars.sort.join.downcase).each do |word|
         words_array << word.name
       end
-    end
     return words_array
   end
 end
+
+
+
 
 
